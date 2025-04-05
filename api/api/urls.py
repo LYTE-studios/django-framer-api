f8de55c8d06a1bd14dadfbfbde981a6a74eb55e6
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2025-04-05 22:32:56 +0200

    fix: login

--- api/api/urls.py
+++ api/api/urls.py
@@ -7,13 +7,17 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # Redirect root to client portal
     path('', RedirectView.as_view(url='/client/', permanent=True)),
 
-    path('client/', include('blogs.urls.client')),
+    # Authentication URLs
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='client/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/client/'), name='logout'),
 
+    path('client/', include('blogs.urls.client')),
     path('api/', include('blogs.urls.api')),
     
     # Django admin
