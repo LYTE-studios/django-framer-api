77d9937e3842f41e1b0666ec7c3ea11fae5b7d75
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2025-04-06 15:32:09 +0200

    fix: namespacing

--- api/api/urls.py
+++ api/api/urls.py
@@ -6,10 +6,12 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     # Client portal URLs
     path('', include('blogs.urls.client', namespace='client')),
+    path('client/', RedirectView.as_view(url='/', permanent=True)),
     
     # API URLs
     path('api/', include('blogs.urls.api')),
--- api/blogs/views/auth.py
+++ api/blogs/views/auth.py
@@ -5,4 +5,4 @@ from ..forms import EmailUserCreationForm
 class RegisterView(CreateView):
     form_class = EmailUserCreationForm
     template_name = 'client/register.html'
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('client:login')
